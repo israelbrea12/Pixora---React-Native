7c8f11e43bbcbd1cb3c764ab7aaa1716fb772fdd
israelbrea12 <israelbrea.02@gmail.com>
2025-08-19 10:44:49 +0200

    Vista nativa DoubleTapImageView para dar me gusta con doble tap hecha y funcional en iOS

---
+++ ios/DoubleTapImageView.h
@@ -0,0 +1,12 @@
+//
+//  DoubleTapImageView.h
+//  PixoraRN
+//
+//  Created by Israel Brea Piñero on 19/8/25.
+//
+
+#ifndef DoubleTapImageView_h
+#define DoubleTapImageView_h
+
+
+#endif /* DoubleTapImageView_h */
---
+++ ios/DoubleTapImageView.m
@@ -0,0 +1,8 @@
+//
+//  DoubleTapImageView.m
+//  PixoraRN
+//
+//  Created by Israel Brea Piñero on 19/8/25.
+//
+
+#import <Foundation/Foundation.h>
